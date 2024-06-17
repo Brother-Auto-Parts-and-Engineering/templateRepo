d83ae7ea17b238227fa2434eefc72df751b661b6
SARAN T <54904456+icesaran3@users.noreply.github.com>
2024-06-17 11:43:31 +0700

    changeDB

--- backend/src/Example/exampleResolver.ts
+++ backend/src/Example/exampleResolver.ts
@@ -1,10 +1,19 @@
+import { equal } from "assert"
 import { getModel } from "../modelUtilities"
 
 export const exampleResolver = {
   Query: {
     example: async (_: any, args: any) => {
       const models = await getModel()
-      const sqlData = await models.new_table.findAll()
+      const sqlData = await models.users.findAll({
+        attributes: [
+          "id",
+          "username",
+          "firstname",
+          "lastname"
+        ]
+      })
+     
       return sqlData
     }
   }
